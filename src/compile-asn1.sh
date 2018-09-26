d9c3df695cc0e2d9843b3bd1cba42929d39804e3
Alberto Leiva Popper <ydahhrk@gmail.com>
2018-09-26 13:54:54 -0500

    Add RFC 6482 (ROA) ASN.1 objects

--- src/compile-asn1.sh
+++ src/compile-asn1.sh
@@ -18,11 +18,19 @@ mkdir -p $OUTPUT_DIR || die "Could not create directory '$OUTPUT_DIR'."
 asn1c -Werror -fcompound-names -D $OUTPUT_DIR \
 		-no-gen-PER -no-gen-example \
 		*.asn1 || die "Compilation failed."
-rm libcmscodec/Makefile.am.libasncodec
 
-echo "Patching include directories from the source. Please wait; this will take a while..."
+echo "Patching include directories from the source. Please wait; this will take a while. (1 minute on my PC.)"
 for MODULE in $OUTPUT_DIR/*.h; do
 	HNAME=$(basename $MODULE)
 	sed -i "s|#include <$HNAME>|#include <libcmscodec/$HNAME>|" $OUTPUT_DIR/*.h $OUTPUT_DIR/*.c
 	sed -i "s|#include \"$HNAME\"|#include \"libcmscodec/$HNAME\"|" $OUTPUT_DIR/*.h $OUTPUT_DIR/*.c
 done
+
+# Patch Makefile.am.libasncodec
+MAKEFILE=$OUTPUT_DIR/Makefile.am.libasncodec
+sed -i 's|ASN_MODULE_HDRS|ASN_MODULE_HEADERS|' $MAKEFILE
+sed -i 's|ASN_MODULE_SRCS|ASN_MODULE_SOURCES|' $MAKEFILE
+sed -i '\|libasncodec_la_CPPFLAGS=-I$(top_srcdir)/libcmscodec/|d' $MAKEFILE
+sed -i 's|libasncodec|libcmscodec|' $MAKEFILE
+sed -i 's|lib_LTLIBRARIES+=libcmscodec.la|lib_LTLIBRARIES=libcmscodec.la|' $MAKEFILE
+printf "\nASN_MODULEdir=@includedir@/libcmscodec\n" >> $MAKEFILE
